4fc00db1934d0592e6f4b356db91e0bd12ecc65a
emrevtnsvr <e.vatansever885@mybvc.ca>
2025-03-21 14:03:48 -0700

    First Actual Update

---
+++ Assets/Scripts/ImageSlicer.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class ImageSlicer : MonoBehaviour
+{
+    [SerializeField] Texture2D sourceImage;
+    [SerializeField] GameObject tilePrefab;
+    [SerializeField] Transform puzzleContainer;
+    GridLayoutGroup gridLayoutGroup;
+    int tileWidth, tileHeight;
+
+    void Start()
+    {
+        SliceImage(3, 3);
+    }
+
+    void SliceImage(int rows,int cols)
+    {
+        gridLayoutGroup = puzzleContainer.GetComponent<GridLayoutGroup>();
+
+        float spacingX = gridLayoutGroup.spacing.x;
+        float spacingY = gridLayoutGroup.spacing.y;
+
+        tileWidth = Mathf.FloorToInt((sourceImage.width - spacingX * (cols-1))/cols);
+        tileHeight = Mathf.FloorToInt((sourceImage.width - spacingY * (rows - 1)) / rows);
+
+        for (int y = 0; y < rows; y++)
+        {
+            for(int x = 0; x < cols; x++)
+            {
+                int correctedY = (rows - 1) - y;
+                if(x ==cols-1 && y == rows - 1)
+                {
+                    continue;
+                }
+
+                CreateTile(x,correctedY);
+            }
+        }
+        AdjustGridCellSize(rows, cols);
+    }
+
+    void CreateTile(int x, int y)
+    {
+        Texture2D tileTexture = new Texture2D(tileWidth, tileHeight);
+        tileTexture.SetPixels(sourceImage.GetPixels(x*tileWidth,y*tileHeight,tileWidth,tileHeight));
+        tileTexture.Apply();
+
+        Sprite tileSprite = Sprite.Create(tileTexture, new Rect(0, 0, tileWidth, tileHeight), new Vector2(0.5f, 0.5f));
+
+        GameObject tile = Instantiate(tilePrefab, puzzleContainer);
+        tile.name = x + "," + y;
+        tile.GetComponent<Image>().sprite = tileSprite; 
+
+    }
+
+    void AdjustGridCellSize(int rows,  int cols)
+    {
+        RectTransform containerRect = puzzleContainer.GetComponent<RectTransform>();
+
+        float spacingX = gridLayoutGroup.spacing.x;
+        float spacingY = gridLayoutGroup.spacing.y;
+
+        float availableWidth = containerRect.rect.width - (spacingX * (cols - 1));
+        float availableHeight = containerRect.rect.height - (spacingY * (rows - 1));
+
+        float cellWidth = availableWidth / cols;
+        float cellHeight = availableHeight / rows;
+
+        gridLayoutGroup.cellSize = new Vector2(cellWidth, cellHeight);
+    }
+}
